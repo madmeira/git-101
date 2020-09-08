a67d94571a3c495106b42f0a6d6eefa57a3099a6
MadMeira <rafael.meira@gmail.com>
2020-09-07 21:29:37 -0300

    Adicionada a nice message
    Adicionada a nice message

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace git_101
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("What a lovely day!");
+            Console.WriteLine("NICE!");
         }
     }
 }
